d66a7a7a85380b7bbd8c5f775b37967022178c17
Hayden-T-Dixon <htdnmb@mst.edu>
2019-09-29 13:57:45 -0500

    Added files for testing JS data types.

---
+++ js/testingDataTypes.js
@@ -0,0 +1,22 @@
+let numtype1 = 1;
+let numtype2 = 1.0;
+let stringtype = "This is a string.";
+let booltype = true;
+let undefinedtype;
+
+let scientificnum1 = 98e4;
+let scientificnum2 = 98e-4;
+
+let maxaccuracy = 15;
+
+let numdivstring = 20/"5";
+
+console.log(numtype1);
+console.log(numtype2);
+console.log(stringtype);
+console.log(booltype);
+console.log(undefinedtype);
+console.log(scientificnum1);
+console.log(scientificnum2);
+console.log(maxaccuracy);
+console.log(numdivstring);
