770ad57b98ed1218a751a76a038e6b17d41a6d4f
Hayden-T-Dixon <htdnmb@mst.edu>
2019-10-19 20:35:41 -0500

    Continued adding more testing to Strings for JS.

--- js/StringTesting.js
+++ js/StringTesting.js
@@ -1,3 +1,26 @@
-alert('This is a string in alert.')
+alert('This is a string in alert.');
 
-console.log('This is a string in console log.')
+console.log('This is a string in console log.');
+
+let string1 = 'Example 1 of a string.';
+var string2 = 'Example 2 of a string.';
+const string3 = 'Example 3 of a string.';
+
+console.log(string1);
+console.log(string2);
+console.log(string3);
+
+var stringconcatenation = 'String part 1;' + ' String part 2.';
+
+console.log(stringconcatenation);
+
+var stringconcat1 = 'String portion 1; ';
+var stringconcat2 = 'String portion 2.';
+
+var stringconcat = stringconcat1 + stringconcat2;
+
+console.log(stringconcat);
+
+stringtemplateliteral = `String: ${stringconcat1}${stringconcat2}`;
+
+console.log(stringtemplateliteral)
